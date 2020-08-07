dc9b94d726c40eaba62971bd5ede633ed0583a94
star1954 <andidiwang@yahoo.com>
2020-08-07 11:00:18 -0700

    Refactored files

--- src/main/java/core/Module_Interface_Event.java
+++ src/main/java/core/Module_Interface_Event.java
@@ -4,7 +4,7 @@ import net.dv8tion.jda.api.events.ReadyEvent;
 import net.dv8tion.jda.api.events.guild.GuildReadyEvent;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 
-public class Module_Event_Interface {
+public class Module_Interface_Event {
     /**
      *              Is called when the bot receives any message, including a command. Use only when necessary, as this will fire every message
      * @param event message received event
@@ -15,7 +15,7 @@ public class Module_Event_Interface {
      *              Is called when the bot receives a command. Default identifier for a command is ! followed by a message
      * @param event message received event
      */
-    public void OnCommand(GuildMessageReceivedEvent event){ }
+    public void OnCommand(GuildMessageReceivedEvent event){}
 
     /**
      *              Is called when the bot is successfully initialized\n
