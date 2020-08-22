c305a243fdfae4eff873a908808d59314afcd4a4
star1954 <andidiwang@yahoo.com>
2020-08-21 18:36:17 -0700

    Reverted to working version

--- src/main/java/core/Module_Event_Interface.java
+++ src/main/java/core/Module_Event_Interface.java
@@ -3,8 +3,11 @@ package core;
 import net.dv8tion.jda.api.events.ReadyEvent;
 import net.dv8tion.jda.api.events.guild.GuildReadyEvent;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import org.apache.log4j.Logger;
 
 public class Module_Event_Interface {
+    protected Logger logger = Logger.getLogger("V-Bot Logger");
+
     /**
      *              Is called when the bot receives any message, including a command. Use only when necessary, as this will fire every message
      * @param event message received event
--- src/main/java/modules/VaultanTracker.java
+++ src/main/java/modules/VaultanTracker.java
@@ -2,13 +2,21 @@ package modules;
 
 import coremodules.Command_Interface;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
-
 public class VaultanTracker extends Command_Interface {
     @Override
     public void OnCommand(GuildMessageReceivedEvent event) {
         super.OnCommand(event);
-        if(args[0] == "!record"){
+        switch (args[0]){
+            case "standGive":
 
+                break;
         }
     }
+
+    @Override
+    public void OnMessage(GuildMessageReceivedEvent event) {
+        super.OnMessage(event);
+        logger.info(event.getAuthor()+" "+event.getChannel()+": "+event.getMessage().getContentRaw());
+    }
 }
+
